c36960f3ca6009fd4dca5ff4c1bd46b26ec75985
waiting7777 <waiting0819@msn.com>
2020-05-26 16:43:07 +0800

    init test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div>Time diff: {{ diff }} ms</div>
+    <div v-for="t in test" :key="t.id">
+      <!-- <v-checkbox v-model="t.value"></v-checkbox> -->
+      <!-- <v-simple-checkbox v-model="t.value" :ripple="false"></v-simple-checkbox> -->
+      <input type="checkbox" v-model="t.value" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { range } from 'lodash'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      test: range(10).map(v => { return { id: v, value: false }}),
+      create: 0,
+      mount: 0,
+      diff: 0
+    }
+  },
+  created() {
+    this.create = new Date().getTime()
+  },
+  mounted() {
+    this.mount = new Date().getTime()
+    this.diff = this.mount - this.create
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
